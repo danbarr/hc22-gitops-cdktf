96ca105549a1103bcb9260055ea592f4b40b01f6
Dan Barr <dan@dbarr.com>
2022-08-30 18:42:18 -0400

    Add variables

--- main.ts
+++ main.ts
@@ -1,17 +1,20 @@
 import { Construct } from "constructs";
 import { App, TerraformStack, TerraformOutput, CloudBackend, NamedCloudWorkspace } from "cdktf";
 import { AwsProvider, ec2 } from "@cdktf/provider-aws";
+import { Tfvars } from "./variables"
 
 class MyStack extends TerraformStack {
   constructor(scope: Construct, name: string) {
     super(scope, name);
 
+    const vars = new Tfvars(this, "main")
+
     new AwsProvider(this, "AWS", {
-      region: "eu-west-1",
+      region: vars.awsRegion,
     });
 
     const instance = new ec2.Instance(this, "compute", {
-      ami: "ami-08a9dc8f6d1c4f806",
+      ami: vars.amiId,
       instanceType: "t3.micro",
       tags: {
         Name: "CDKTF-Demo"
